8dea94c5db6305be657f2e82a0bdd2ac7498b5a9
Volodymir <v_admin@ukr.net>
2023-07-03 16:55:57 +0300

    add register

--- test_register.py
+++ test_register.py
@@ -6,30 +6,28 @@ from dotenv import load_dotenv
 from aiogram import F
 
 
-
 load_dotenv()
 
 bot: Bot=Bot(token = os.environ.get('BOT_TOKEN'))
 dp: Dispatcher=Dispatcher()
 
-
-@dp.message(Command(commands=['start']))
 async def start_message(message: Message):
     await message.answer("Hello")
 
-@dp.message(Command(commands=['help']))
 async def help_message(message: Message):
     await message.answer("Can I help you???")
 
-@dp.message(F.photo)   # F.audio F.voice F.document
 async def photo_message(message: Message):
     print(message)
     await message.reply_photo(message.photo[-1].file_id)
 
-@dp.message()
 async def else_message(message: Message):
     await message.answer("Nice to meet you")
 
+dp.message.register(start_message, Command(commands=['start']))
+dp.message.register(help_message, Command(commands=['help']))
+dp.message.register(photo_message, F.photo)  # F.audio F.voice F.document
+dp.message.register(else_message)
 
 if __name__ == "__main__":
     dp.run_polling(bot)
