93f6c484c6b0df8879cb666cd0437d36220948f0
Volodymir <v_admin@ukr.net>
2023-07-03 14:40:08 +0300

    add handlers

--- test.py
+++ test.py
@@ -16,6 +16,14 @@ dp: Dispatcher=Dispatcher()
 async def start_message(message: Message):
     await message.answer("Hello")
 
+@dp.message(Command(commands=['help']))
+async def help_message(message: Message):
+    await message.answer("Can I help you???")
+
+@dp.message()
+async def else_message(message: Message):
+    await message.answer("Nice to meet you")
+
 
 if __name__ == "__main__":
     dp.run_polling(bot)
