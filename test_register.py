df7dcdd15de45afe566f46b2dca280cd1e860836
Volodymir <v_admin@ukr.net>
2023-07-04 11:30:47 +0300

    PEP8 formater

--- test_register.py
+++ test_register.py
@@ -21,12 +21,18 @@ async def photo_message(message: Message):
     print(message)
     await message.reply_photo(message.photo[-1].file_id)
 
+
+async def audio_message(message: Message):
+    print(message)
+    await message.answer_voice(message.voice.file_id)
+
 async def else_message(message: Message):
     await message.answer("Nice to meet you")
 
 dp.message.register(start_message, Command(commands=['start']))
 dp.message.register(help_message, Command(commands=['help']))
 dp.message.register(photo_message, F.photo)  # F.audio F.voice F.document
+dp.message.register(audio_message, F.voice)  # F.audio F.voice F.document
 dp.message.register(else_message)
 
 if __name__ == "__main__":
